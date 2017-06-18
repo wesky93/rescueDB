826be6f50edb9563c830e140fca7f4b2d75866e5
sinsky <wesky93@gmail.com>
2017-06-19 00:27:26 +0900

    url 추가

--- record/views.py
+++ record/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
 
 # Create your views here.
+class Home():
+    """
+    메인 버튼 노출 뷰
+    """
+    pass
+
+class Input():
+    """
+    구조 기록 입력 뷰
+    """
+    pass
+
+class Records():
+    """
+    구조기록 목록 뷰
+    """
+
+    pass
+
+class Detail():
+    """
+    구조 정보 상세 뷰뷰
+   """
+    pass
+
