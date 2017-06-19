937d97d48fdfba6abb6d0662cfb910b28920c7a2
sinsky <wesky93@gmail.com>
2017-06-19 09:33:03 +0900

    기본 모델 구조 추가&어드민 페이지에 할당

--- record/admin.py
+++ record/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import RescueInfo
 
 # Register your models here.
+
+@admin.register(RescueInfo)
+class RescueInfoAdmin(admin.ModelAdmin):
+    pass
--- record/models.py
+++ record/models.py
@@ -3,22 +3,19 @@ from django.db import models
 # Create your models here.
 # 이름 나이 생년월일 주소 핸드폰번호 사고위치 증상 사고원인 구조내용 구조사진
 class RescueInfo(models.Model):
-    """
-    블로그 포스트 모델
-    """
-    name = models.ForeignKey('Category', blank=True, null=True, verbose_name='카테고리')
-    age = models.CharField(verbose_name='제목', max_length=60)
-    burth = models.DateTimeField(verbose_name='생일')
-    address = models.CharField(verbose_name='요약', max_length=160, blank=True, null=True,
-                                   help_text="포스트를 sns에 공유하거나 검색에서 노출될 포스트에 대한 요약입니다.")
-    phone = ''
+    name = models.CharField( verbose_name='이름', max_length=6 )
+    age = models.IntegerField(verbose_name='나이')
+    burth = models.DateTimeField(verbose_name='생년월일')
+    address = models.CharField(verbose_name='요약', max_length=40, blank=True, null=True,)
+    phone = models.CharField( verbose_name='연락처', max_length=11,blank=True, null=True,
+                              help_text="'-'없이 입력하세요 ex) 01012341234")
 
     # 사고위치
-    location = models.CharField(verbose_name='사고위치', max_length=60)
-    symptom = ""
-    cause = ''
+    location = models.CharField(verbose_name='사고위치', max_length=10)
+    symptom = models.CharField(verbose_name='증상', max_length=20)
+    cause = models.CharField(verbose_name='원인', max_length=30)
     content = models.TextField(verbose_name='구조 내용')
-    picture = ''
+    picture = models.ImageField()
 
     objects = models.Manager()
 
