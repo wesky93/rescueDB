1e9ee17aeb328f133221cef66c887a81e17341a5
sinsky <wesky93@gmail.com>
2017-07-02 00:18:31 +0900

    업로드 파일 없을시 기록 추가 안되는 버그 수정

--- record/forms.py
+++ record/forms.py
@@ -8,7 +8,7 @@ class RescueForm(forms.ModelForm):
         model = Rescue
         fields = ['name', 'age', 'burth','address','phone','location','symptom','cause','content']  # not attachments!
 
-    files = MultiFileField(min_num=1, max_num=5, max_file_size=1024*1024*6)
+    files = MultiFileField(required=False,min_num=0, max_num=5, max_file_size=1024*1024*6)
 
     def save(self, commit=True):
         instance = super(RescueForm, self).save(commit)
